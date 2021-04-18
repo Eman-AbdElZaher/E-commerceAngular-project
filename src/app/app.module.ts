619706275e35a2837ef824eaf7f90e95a6453242
Doaa <doaagamal16.77@gmail.com>
2021-04-18 22:01:43 +0200

    Doaa Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstcomComponent } from './firstcom/firstcom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstcomComponent
   ],
   imports: [
     BrowserModule,
