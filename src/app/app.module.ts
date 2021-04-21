8ed974c6980cae55f7dc8560f16d49ff8debcd86
Eman-AbdElZaher <e.m.abdelzaher@gmail.com>
2021-04-22 00:45:55 +0200

    add home layout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { FirstcomComponent } from './firstcom/firstcom.component';
-
-import { ProductComponent } from './Components/product/product.component';
+import { HeaderComponent } from './Layout/header/header.component';
+import { FooterComponent } from './Layout/footer/footer.component';
+import { BannerComponent } from './Layout/banner/banner.component';
+import { HomeComponent } from './homePage/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-
-    FirstcomComponent,
-
-    ProductComponent
+      HeaderComponent,
+      FooterComponent,
+      BannerComponent,
+      HomeComponent
 
   ],
   imports: [
--- src/app/homePage/home/home.component.spec.ts
+++ src/app/homePage/home/home.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FirstcomComponent } from './firstcom.component';
+import { HomeComponent } from './home.component';
 
-describe('FirstcomComponent', () => {
-  let component: FirstcomComponent;
-  let fixture: ComponentFixture<FirstcomComponent>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FirstcomComponent ]
+      declarations: [ HomeComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FirstcomComponent);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
